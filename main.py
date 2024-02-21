a86435468e2f7e38001552111db4eaf2b3e59650
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-21 11:43:19 -0600

    Correct the frog's movement to account for buttons being held down, and proper spacing of jumps

--- main.py
+++ main.py
@@ -1,53 +1,61 @@
 import pygame
 from pygame.locals import *
-import sys
 from frog import Frog
 
 pygame.init()
 
 # Screen dimensions
-SCREEN_WIDTH = 750
-SCREEN_HEIGHT = 850
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Frogger")
 
 # Image variables
 background = pygame.image.load('img/background.png')
 fitted_background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
-frog = Frog(SCREEN_WIDTH // 2, (SCREEN_HEIGHT // 2) + 400)
+frog = Frog(SCREEN_WIDTH // 2, (SCREEN_HEIGHT // 2) + 370)
 
+# Button booleans
+key_pressed = {
+  pygame.K_UP: False,
+  pygame.K_DOWN: False,
+  pygame.K_LEFT: False,
+  pygame.K_RIGHT: False
+}
 
 # Main Game Loop
 running = True
 while running:
 
-  # render image variables
-  screen.blit(fitted_background, (0,0))
-  screen.blit(frog.image, frog.rect)
-
-  # Get keys pressed
-  keys = pygame.key.get_pressed()
-
-  # Move the frog
-  if keys[pygame.K_UP]:
-    frog.update_direction("up")
-    frog.move(0, -5)
-  elif keys[pygame.K_DOWN]:
-    frog.update_direction("down")
-    frog.move(0, 5)
-  elif keys[pygame.K_LEFT]:
-    frog.update_direction("left")
-    frog.move(-5, 0)
-  elif keys[pygame.K_RIGHT]:
-    frog.update_direction("right")
-    frog.move(5, 0)
-
   # exiting the game
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
-      
-  
+    # Button limiters
+    elif event.type == pygame.KEYDOWN:
+      if event.key in key_pressed:
+        key_pressed[event.key] = True
+        # Move the frog
+        if key_pressed[pygame.K_UP]:
+          frog.update_direction("up")
+          frog.move(0, -62)
+        elif key_pressed[pygame.K_DOWN]:
+          frog.update_direction("down")
+          frog.move(0, 62)
+        elif key_pressed[pygame.K_LEFT]:
+          frog.update_direction("left")
+          frog.move(-62, 0)
+        elif key_pressed[pygame.K_RIGHT]:
+          frog.update_direction("right")
+          frog.move(62, 0)
+    elif event.type == pygame.KEYUP:
+      if event.key in key_pressed:
+        key_pressed[event.key] = False
+
+  # render image variables
+  screen.blit(fitted_background, (0,0))
+  screen.blit(frog.image, frog.rect)
+
   pygame.display.update()
 
 pygame.quit()
